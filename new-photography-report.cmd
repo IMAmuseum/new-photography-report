5515dfde9129f445f3b7a4f89e7bfbf12120d272
Sami Norling <sami.norling@gmail.com>
2023-03-07 07:48:47 -0500

    Update Saxon HE version

--- new-photography-report.cmd
+++ new-photography-report.cmd
@@ -17,7 +17,7 @@ REM cURL GET call to Piction API for session verification, output as XML\SURL.xm
 curl --insecure https://digitalmedia.imamuseum.org/r/st//piction_login/USERNAME/%username%/PASSWORD/%password% -o "XML\SURL.xml"
 
 REM call saxon to run XSLT\SURLtransform.xsl on XML\SURL.xml to update CMD\PictionCall.cmd
-java -jar C:\saxon\saxon9he.jar -s:XML\SURL.xml -xsl:XSLT\NewPhotography_SURLtransform.xsl -o:CMD\PictionCall.cmd
+java -jar C:\saxon\saxon-he-11.4.jar -s:XML\SURL.xml -xsl:XSLT\NewPhotography_SURLtransform.xsl -o:CMD\PictionCall.cmd
 
 REM run PictionCall.cmd to set %PictionCall% and %Date% variables
 call CMD\PictionCall.cmd
@@ -26,19 +26,19 @@ REM cURL GET call to Piction API to retrieve 7 days of new photography metadata,
 curl --insecure %PictionCall% -o "CurrentReport\NewPhotographyOutput_%Date%.xml"
 
 REM call saxon to run XSLT\NewPhotography_HTMLtransform.xsl on CurrentReport\NewPhotographyOutput_%Date%.xml, output as CurrentReport\NewPhototgraphyReport_%Date%
-java -jar C:\saxon\saxon9he.jar -s:CurrentReport\NewPhotographyOutput_%Date%.xml -xsl:XSLT\NewPhotography_HTMLtransform.xsl -o:CurrentReport\NewPhotographyReport_%Date%.html
+java -jar C:\saxon\saxon-he-11.4.jar -s:CurrentReport\NewPhotographyOutput_%Date%.xml -xsl:XSLT\NewPhotography_HTMLtransform.xsl -o:CurrentReport\NewPhotographyReport_%Date%.html
 
 REM call saxon to run XSLT\NewPhotography_IRNlistTransform.xsl on CurrentReport\NewPhototgraphyOutput_%Date%.xml, output as CurrentReport\NewPhotography_IRNs_%Date%.csv
-java -jar C:\saxon\saxon9he.jar -s:CurrentReport\NewPhotographyOutput_%Date%.xml -xsl:XSLT\NewPhotography_IRNlistTransform.xsl -o:CurrentReport\NewPhotography_IRNs_%Date%.csv
+java -jar C:\saxon\saxon-he-11.4.jar -s:CurrentReport\NewPhotographyOutput_%Date%.xml -xsl:XSLT\NewPhotography_IRNlistTransform.xsl -o:CurrentReport\NewPhotography_IRNs_%Date%.csv
 
 REM call saxon to run XSLT\NewPhotography_KeywordTransform.xsl on CurrentReport\NewPhotographyOutput_%Date%.xml, output as CurrentReport\NewPhotography_Keywords_%Date%.txt
-java -jar C:\saxon\saxon9he.jar -s:CurrentReport\NewPhotographyOutput_%Date%.xml -xsl:XSLT\NewPhotography_KeywordTransform.xsl -o:CurrentReport\NewPhotography_Keywords_%Date%.txt
+java -jar C:\saxon\saxon-he-11.4.jar -s:CurrentReport\NewPhotographyOutput_%Date%.xml -xsl:XSLT\NewPhotography_KeywordTransform.xsl -o:CurrentReport\NewPhotography_Keywords_%Date%.txt
 
 REM generate word cloud and save as CurrentReport\NewPhotography_WordCloud_%Date%.png
 java -jar ibm-word-cloud.jar -c IBMconfig.txt -w 1600 -h 1600 -i CurrentReport\NewPhotography_Keywords_%Date%.txt > CurrentReport\NewPhotography_WordCloud_%Date%.png
 
 REM call saxon to run XSLT\NewPhotography_ImageURLtransform.xsl on CurrentReport\NewPhotographyOutput_%Date%.xml, output as CMD\DownloadImages.cmd
-java -jar C:\saxon\saxon9he.jar -s:CurrentReport\NewPhotographyOutput_%Date%.xml -xsl:XSLT\NewPhotography_ImageURLtransform.xsl -o:CMD\DownloadImages.cmd
+java -jar C:\saxon\saxon-he-11.4.jar -s:CurrentReport\NewPhotographyOutput_%Date%.xml -xsl:XSLT\NewPhotography_ImageURLtransform.xsl -o:CMD\DownloadImages.cmd
 
 REM change directory to CurrentReport
 cd CurrentReport
